70b68ae4001fc58fd2f5e9658825efd343236bd2
yunuo468@gmail.com <yunuo468@gmail.com>
2022-10-25 00:13:06 +0900

    add 오류 수정

--- Course Registration/Add.cpp
+++ Course Registration/Add.cpp
@@ -14,6 +14,11 @@
 #include "SubjectData.h"
 using namespace std;
 
+string wtos(const wstring& _src) {
+	USES_CONVERSION;
+	return string(W2A(_src.c_str()));
+};
+
 void Add(string command, string studentid, string studentname) {
 	string id;
 	string mileage;
@@ -22,13 +27,13 @@ void Add(string command, string studentid, string studentname) {
 	string token;
 	string check;
 	wstring line;
-	vector<pair<int,string>> find_time;
-	queue < pair<int,string>> find_times;
-	vector<int>found_time;
+	vector<pair<string, string>> find_time;
+	queue < pair<string, string>> find_times;
+	vector<string>found_time;
 	vector<string> compare_times;
 	int mileage_sum;
-	bool c1, c2, c3, c4,c5;
-	c1 = c2 = c3 = c4 =c5= true;
+	bool c1, c2, c3, c4, c5;
+	c1 = c2 = c3 = c4 = c5 = true;
 
 	while (getline(ss, token, '/'))
 	{
@@ -73,7 +78,7 @@ void Add(string command, string studentid, string studentname) {
 				c1 = false;
 				//cout << "오류 : 이미 추가한 과목입니다"<<endl;
 			}
-			find_time.push_back(make_pair(stoi(tmpid),Subject[stoi(tmpid)]->time)); // 과목 시간대 저장
+			find_time.push_back(make_pair(wtos(tmpid), Subject[stoi(tmpid)]->time)); // 과목 시간대 저장
 			credit_sum += Subject[stoi(tmpid)]->credit;
 		}
 		f1.close();
@@ -96,10 +101,10 @@ void Add(string command, string studentid, string studentname) {
 		ss.str(find_time[i].second);
 		while (getline(ss, token, ','))
 		{
-			find_times.push(make_pair(find_time[i].first,token));
+			find_times.push(make_pair(find_time[i].first, token));
 		}
 	}
-	while(!find_times.empty()){
+	while (!find_times.empty()) {
 		for (int j = 0; j < compare_times.size(); j++) {
 			string now = find_times.front().second;
 			//cout << now.substr(0, 2) << " " << compare_times[j].substr(0, 2) << endl;
@@ -108,8 +113,11 @@ void Add(string command, string studentid, string studentname) {
 				int f_right = stoi(now.substr(5, 2));
 				int c_left = stoi(compare_times[j].substr(2, 2));
 				int c_right = stoi(compare_times[j].substr(5, 2));
-				//cout << f_left << " " << f_right << " " << c_left << "  " << c_right << endl;
-				if ((f_left >= c_left && f_left < c_right) || (f_right > c_left && f_right <= c_right)) {
+				/*cout << f_left << " " << f_right << " " << c_left << "  " << c_right << endl;
+				cout << (f_left >= c_left && f_left < c_right) << (f_right > c_left && f_right <= c_right)<<
+					(f_left <= c_left && f_right >= c_right)<< (f_left >= c_left && f_left <= c_right && f_right >= c_left && f_right <= c_right) << endl;*/
+				if ((f_left >= c_left && f_left < c_right && f_right >= c_right) || (f_left <= c_left && f_right > c_left && f_right <= c_right)
+					|| (f_left <= c_left && f_right >= c_right) || (f_left >= c_left && f_left <= c_right && f_right >= c_left && f_right <= c_right)) {
 					//cout << "오류 : 추가하려는 과목과 강의시간이 겹치는 과목이 이미 추가되었습니다." << endl;
 					c3 = false;
 					found_time.push_back(find_times.front().first);
@@ -154,15 +162,15 @@ void Add(string command, string studentid, string studentname) {
 		else return;
 	}
 	else {
-		if(!c1) cout << "오류 : 이미 추가한 과목입니다" << endl;
-		if(!c2)cout << "최대 이수 학점(18학점)을 초과하였습니다.";
-		if (!c3) {
+		if (!c1) cout << "오류 : 이미 추가한 과목입니다" << endl;
+		if (!c2)cout << "최대 이수 학점(18학점)을 초과하였습니다.";
+		if (c1 && !c3) {
 			sort(found_time.begin(), found_time.end());
 			found_time.erase(unique(found_time.begin(), found_time.end()), found_time.end());
 			cout << "오류 : 추가하려는 과목과 강의시간이 겹치는 과목이 이미 추가되었습니다." << endl;
 			cout << "[" << id << "] " << Subject[stoi(id)]->name << " " << Subject[stoi(id)]->time << endl;
 			for (int i = 0; i < found_time.size(); i++) {
-				cout << "[" << found_time[i] << "] " << Subject[found_time[i]]->name << " " << Subject[found_time[i]]->time << endl;
+				cout << "[" << found_time[i] << "] " << Subject[stoi(found_time[i])]->name << " " << Subject[stoi(found_time[i])]->time << endl;
 			}
 		}
 		if (!c4)cout << "오류 : 신청한 마일리지가 남은 마일리지를 초과합니다." << endl;
